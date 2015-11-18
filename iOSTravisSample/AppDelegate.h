ef0fd2c22b7117f5b203edda4ba3f7dd5d6f1678
Joseph Liu <joseph.hayato@gmail.com>
2015-11-18 15:54:55 +0800

    Initial Commit

---
+++ iOSTravisSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTravisSample
+//
+//  Created by natanovia on 2015/11/18.
+//  Copyright © 2015年 suwa-hayato.info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
